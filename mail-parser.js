4717269365d1ff5126d305be7fe18606af6902df
Charles Collas <collas.charles@gmail.com>
2014-10-11 23:44:47 +0200

    Update mail-parser.js

--- mail-parser.js
+++ mail-parser.js
@@ -5,7 +5,7 @@ exports.parse_recipient_type = function(recipient_list){
     'cc': [],
     'bcc': []
   }
-  for(var i=0;i<recipient_list.length;++i){
+  for (var i=0; i<recipient_list.length; ++i) {
     var parsed = recipient_list[i].split(':');
     (parsed.length > 1) ? return_vals[parsed[0]].push(parsed[1])
       : return_vals['to'].push(parsed[0]);
